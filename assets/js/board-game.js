c4d69d9655d637e0ae4e6eb700015a6edbe69e4f
LeoHugues <leo.hugues@hotmail.fr>
2020-07-14 15:43:21 +0200

    fix timer

--- assets/js/board-game.js
+++ assets/js/board-game.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 
 const PAIR_NUMBER = 18;
 
-const TIME_LIMIT = 10;
+const TIME_LIMIT = 180;
 const FULL_DASH_ARRAY = 283;
 const WARNING_THRESHOLD = 60;
 const ALERT_THRESHOLD = 30;
